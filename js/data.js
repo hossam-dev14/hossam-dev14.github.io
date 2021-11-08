e728bf9dd22776ca85fb187f3762a30332796db9
hossamdev <73648971+hossam-dev14@users.noreply.github.com>
2021-11-08 16:22:56 +0100

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,152 @@
+
+
+skills = 
+[
+  {
+    "id": 1,
+    "title" : "HTML",
+    "percent" : 80
+  },
+  {
+    "id": 2,
+    "title" : "CSS",
+    "percent" : 73
+  },
+  {
+    "id": 3,
+    "title" : "JavaScript",
+    "percent" : 70
+  },
+  {
+    "id": 4,
+    "title" : "Bootstrap",
+    "percent" : 85
+  },
+  {
+    "id": 5,
+    "title" : "React",
+    "percent" : 50
+  }
+];
+
+knowledges = 
+[
+  {
+    "id": 1,
+    "title" : "ES6",
+  },
+  {
+    "id": 2,
+    "title" : "RegEx",
+  },
+  {
+    "id": 3,
+    "title" : "SEO",
+  },
+  {
+    "id": 4,
+    "title" : "APIs",
+  },
+  {
+    "id": 5,
+    "title" : "NPM",
+  },
+  {
+    "id": 6,
+    "title" : "Hosting & Domains",
+  },
+  {
+    "id": 7,
+    "title" : "PhotoShop",
+  },
+  {
+    "id": 8,
+    "title" : "JSON & AJAX",
+  },
+  {
+    "id": 9,
+    "title" : "jQuery",
+  }
+];
+
+
+// certifications = 
+// [
+//   {
+//     "id": 1,
+//     "img" : "ES6",
+//     "name" : "ES6",
+//     "source" : "https://www.sololearn.com/",
+//     "date" : "Febrary 27, 2019",
+//     "verify_cert" : "ES6",
+//     "profile" : "ES6",
+//   },
+// ]
+
+
+works = 
+[
+  {
+    "id": 1,
+    "img" : "https://user-images.githubusercontent.com/73648971/112833177-473f0400-908e-11eb-953a-a8d03d03fc33.jpg",
+    "alt" : "animal-blog",
+    "title" : "Animal Blog",
+    "prev_img" : "images/portfolio/animal-blog-preview.jpg",
+    "preview" : "https://hossam-dev14.github.io/Animal-Blog/",
+    "github" : "https://github.com/hossam-dev14/Animal-Blog/"
+  },
+  {
+    "id": 2,
+    "img" : "https://user-images.githubusercontent.com/73648971/113672496-c5fbf880-96af-11eb-8c37-9484fcd89f90.png",
+    "alt" : "coming-soon-page",
+    "title" : "Coming Soon Page",
+    "prev_img" : "https://user-images.githubusercontent.com/73648971/113672496-c5fbf880-96af-11eb-8c37-9484fcd89f90.png",
+    "preview" : "https://coming-soon-dev14.netlify.app/",
+    "github" : "https://github.com/hossam-dev14/Coming-Soon/"
+  },
+  {
+    "id": 3,
+    "img" : "https://user-images.githubusercontent.com/73648971/112232119-5100db80-8c38-11eb-9403-1c7396390c4b.png",
+    "alt" : "portfolio",
+    "title" : "Portfolio",
+    "prev_img" : "images/portfolio/portfolio-preview.jpg",
+    "preview" : "https://hossam-dev14.github.io/Portfolio/",
+    "github" : "https://github.com/hossam-dev14/Portfolio/"
+  },
+  {
+    "id": 4,
+    "img" : "https://user-images.githubusercontent.com/73648971/113200902-cc7a1280-9260-11eb-8bcb-f034d6bdb924.PNG",
+    "alt" : "404-not-found",
+    "title" : "404 Not Found",
+    "prev_img" : "https://user-images.githubusercontent.com/73648971/113200902-cc7a1280-9260-11eb-8bcb-f034d6bdb924.PNG",
+    "preview" : "https://404-Not-Found-dev14.netlify.app/",
+    "github" : "https://github.com/hossam-dev14/404-Not-Found/"
+  },
+  {
+    "id": 5,
+    "img" : "images/portfolio/recipe-page.jpg",
+    "alt" : "recipe-page",
+    "title" : "Recipe Page",
+    "prev_img" : "https://user-images.githubusercontent.com/73648971/113791183-00f73e00-973b-11eb-9eda-0fc6678c23df.png",
+    "preview" : "https://recipe-page-dev14.netlify.app",
+    "github" : "https://github.com/hossam-dev14/Recipe-Page"
+  },
+  {
+    "id": 6,
+    "img" : "https://user-images.githubusercontent.com/73648971/113441519-5a333a80-93e6-11eb-884e-9f169bc5b852.png",
+    "alt" : "my-team-pag",
+    "title" : "My Team Page",
+    "prev_img" : "https://user-images.githubusercontent.com/73648971/113441519-5a333a80-93e6-11eb-884e-9f169bc5b852.png",
+    "preview" : "https://my-team-page-dev14.netlify.app",
+    "github" : "https://github.com/hossam-dev14/my-team-page"
+  },
+  {
+  "id": 7,
+  "img" : "images/portfolio/beard-oil-landing-page.png",
+  "alt" : "beard-oil-landing-page",    
+  "title" : "Beard Oil Landing Page",
+  "prev_img" : "https://user-images.githubusercontent.com/73648971/118375094-0ef87400-b5af-11eb-83c6-147f085b913f.png",
+  "preview" : "https://beard-oil-landing-page-dev14.netlify.app",
+  "github" : "https://github.com/hossam-dev14/beard-oil-landing-page"
+  },
+]
