f6ad1908a34d9c5433cd0ef9fc7206956da5699f
hossamdev <73648971+hossam-dev14@users.noreply.github.com>
2022-02-01 14:26:44 +0100

    Add files via upload

--- js/data.js
+++ js/data.js
@@ -70,6 +70,60 @@ knowledges =
 ];
 
 
+certifications = 
+[
+  {
+    "id": 1,
+    "name": "Java Course",
+    "img" : "https://user-images.githubusercontent.com/73648971/151953603-162b7484-846c-4fbb-8499-dfb3fbd1174c.png",
+    "alt" : "responsive-web-design",
+    "src": "SOLOLEARN",
+    "date": "Feb 27, 2019",
+    "verifyCert": "https://www.sololearn.com/Certificate/1068-4539606/jpg/",
+  },
+  {
+    "id": 2,
+    "name": "Responsive Web Design",
+    "img" : "https://user-images.githubusercontent.com/73648971/151952730-c617e186-0bcb-468e-b8d1-351c826ef5b6.jpg",
+    "alt" : "responsive-web-design",
+    "src": "FreeCodeCamp",
+    "date": "Dec 11, 2020",
+    "verifyCert": "https://freecodecamp.org/certification/hossam-dev14/responsive-web-design/",
+    "certID":""
+  },
+  {
+    "id": 3,
+    "name": "Computer Thinking",
+    "img" : "https://user-images.githubusercontent.com/73648971/151952391-6e616e1f-9488-49bf-a38b-8b28d91ba506.PNG",
+    "alt" : "computer-thinking",
+    "src": "OpenClassRooms",
+    "date": "Dec 10, 2021",
+    "verifyCert": "https://openclassrooms.com/en/course-certificates/2907010334",
+    "certID":"2907010334"
+  },
+  {
+    "id": 3,
+    "name": "Build web apps with ReactJS",
+    "img" : "https://user-images.githubusercontent.com/73648971/151952297-a7f4a7ba-ff59-4619-a873-d8930322d737.png",
+    "alt" : "build-web-apps-with-react",
+    "src": "OpenClassRooms",
+    "date": "Dec 26, 2021",
+    "verifyCert": "https://openclassrooms.com/en/course-certificates/4108820132",
+    "certID":"4108820132"
+  },
+  {
+    "id": 4,
+    "name": "Front-End Web UI Frameworks and Tools: Bootstrap 4",
+    "img" : "https://user-images.githubusercontent.com/73648971/151952865-7c015e7d-5d02-4ed9-8dd4-e80ea54ba924.png",
+    "alt" : "frontend-web-ui",
+    "src": "Coursera",
+    "date": "Jan 27, 2022",
+    "verifyCert": "https://www.coursera.org/verify/EWK8YY9A3NZX",
+    "certID":""
+  }
+];
+
+
 works = 
 [
   {
@@ -128,11 +182,11 @@ works =
   },
   {
   "id": 7,
-  "img" : "images/portfolio/beard-oil-landing-page.png",
+  "img" : "https://user-images.githubusercontent.com/73648971/151848731-29ac4955-63d4-4b39-904e-0f75611c2b56.png",
   "alt" : "beard-oil-landing-page",    
   "title" : "Beard Oil Landing Page",
   "prev_img" : "https://user-images.githubusercontent.com/73648971/118375094-0ef87400-b5af-11eb-83c6-147f085b913f.png",
   "preview" : "https://beard-oil-landing-page-dev14.netlify.app",
   "github" : "https://github.com/hossam-dev14/beard-oil-landing-page"
-  },
+  }
 ]
