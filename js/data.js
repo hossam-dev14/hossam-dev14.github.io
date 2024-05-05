86cc67b98eb574245935754addacf5a6683bfd56
hossamdev <hossamdev14@gmail.com>
2024-05-05 11:56:07 +0100

    Added techinnovate project

--- js/data.js
+++ js/data.js
@@ -122,7 +122,7 @@ certifications =
     "certID":"2907010334"
   },
   {
-    "id": 3,
+    "id": 4,
     "name": "Build web apps with ReactJS",
     "img" : "https://user-images.githubusercontent.com/73648971/151952297-a7f4a7ba-ff59-4619-a873-d8930322d737.png",
     "alt" : "build-web-apps-with-react",
@@ -132,7 +132,7 @@ certifications =
     "certID":"4108820132"
   },
   {
-    "id": 4,
+    "id": 5,
     "name": "Front-End Web UI Frameworks and Tools: Bootstrap 4",
     "img" : "https://user-images.githubusercontent.com/73648971/151952865-7c015e7d-5d02-4ed9-8dd4-e80ea54ba924.png",
     "alt" : "frontend-web-ui",
@@ -148,12 +148,13 @@ works =
 [
   {
     "id": 1,
-    "img" : "https://user-images.githubusercontent.com/73648971/112833177-473f0400-908e-11eb-953a-a8d03d03fc33.jpg",
-    "alt" : "animal-blog",
-    "title" : "Animal Blog",
-    "prev_img" : "https://user-images.githubusercontent.com/73648971/151997109-5a7fdbbb-336e-4040-a07f-2a31bcd3a07c.jpg",
-    "preview" : "https://hossam-dev14.github.io/Animal-Blog/",
-    "github" : "https://github.com/hossam-dev14/Animal-Blog/"
+    "img" : "https://github-production-user-asset-6210df.s3.amazonaws.com/73648971/328004735-28227b48-5346-4499-94ea-f95b53d09b6d.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVCODYLSA53PQK4ZA%2F20240505%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240505T102338Z&X-Amz-Expires=300&X-Amz-Signature=3deb25780fe894c5f3933a3886ae3117436ca15db95d2a6d761d884785bb1ca9&X-Amz-SignedHeaders=host&actor_id=0&key_id=0&repo_id=763128391",
+    "github" : "https://github.com/hossam-dev14/ImmoMarket",
+    "alt" : "ImmoMarket",
+    "title" : "Teck Innovate",
+    "desx": "A dynamic real estate market, For real estate advertisements and rental of apartments and houses",
+    "prev_img" : "https://github-production-user-asset-6210df.s3.amazonaws.com/73648971/328004735-28227b48-5346-4499-94ea-f95b53d09b6d.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVCODYLSA53PQK4ZA%2F20240505%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240505T102338Z&X-Amz-Expires=300&X-Amz-Signature=3deb25780fe894c5f3933a3886ae3117436ca15db95d2a6d761d884785bb1ca9&X-Amz-SignedHeaders=host&actor_id=0&key_id=0&repo_id=763128391",
+    "preview" : "https://tech-innovate-14.vercel.app/"
   },
   {
     "id": 2,
@@ -164,6 +165,15 @@ works =
     "prev_img" : "https://user-images.githubusercontent.com/73648971/113672496-c5fbf880-96af-11eb-8c37-9484fcd89f90.png",
     "preview" : "",
     "github" : "https://github.com/hossam-dev14/ImmoMarket"
+  },
+  {
+    "id": 3,
+    "img" : "https://user-images.githubusercontent.com/73648971/112833177-473f0400-908e-11eb-953a-a8d03d03fc33.jpg",
+    "alt" : "animal-blog",
+    "title" : "Animal Blog",
+    "prev_img" : "https://user-images.githubusercontent.com/73648971/151997109-5a7fdbbb-336e-4040-a07f-2a31bcd3a07c.jpg",
+    "preview" : "https://hossam-dev14.github.io/Animal-Blog/",
+    "github" : "https://github.com/hossam-dev14/Animal-Blog/"
   },
   {
     "id": 3,
