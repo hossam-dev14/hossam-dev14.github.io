0277d19ffb32d8725c176905a6eed556a6993be0
hossamdev <hossamdev14@gmail.com>
2024-07-20 20:25:53 +0100

    Updated certification and about section

--- js/data.js
+++ js/data.js
@@ -1,231 +1,213 @@
-
-
-skills = 
-[
+skills = [
   {
-    "id": 1,
-    "title" : "HTML & CSS",
-    "percent" : 85
+    id: 1,
+    title: "HTML & CSS",
+    percent: 85,
   },
   {
-    "id": 2,
-    "title" : "JavaScript",
-    "percent" : 77
+    id: 2,
+    title: "JavaScript",
+    percent: 77,
   },
   {
-    "id": 3,
-    "title" : "ReactJS & NextJS",
-    "percent" : 70
+    id: 3,
+    title: "ReactJS & NextJS",
+    percent: 70,
   },
   {
-    "id": 4,
-    "title" : "NodeJS & ExpressJS",
-    "percent" : 70
+    id: 4,
+    title: "NodeJS & ExpressJS",
+    percent: 70,
   },
   {
-    "id": 5,
-    "title" : "MongoDB",
-    "percent" : 65
-  }
-];
-
-knowledges = 
-[
-  {
-    "id": 1,
-    "title" : "Git & Github",
-  },
-  {
-    "id": 2,
-    "title" : "SEO",
-  },
-  {
-    "id": 3,
-    "title" : "APIs",
-  },
-  {
-    "id": 4,
-    "title" : "Hosting & Domains",
-  },
-  {
-    "id": 5,
-    "title" : "Trello & Jira",
-  },
-  {
-    "id": 6,
-    "title" : "JSON & AJAX",
-  },
-  {
-    "id": 7,
-    "title" : "DOM & BOM",
-  },
-  {
-    "id": 8,
-    "title" : "jQuery",
-  },
-  {
-    "id": 9,
-    "title" : "Bootstrap",
-  },
-  {
-    "id": 10,
-    "title" : "TailwindCSS",
-  },
-  {
-    "id": 11,
-    "title" : "JWT",
-  },
-  {
-    "id": 12,
-    "title" : "Postman",
-  },
-  {
-    "id": 13,
-    "title" : "Docker",
-  },
-  {
-    "id": 14,
-    "title" : "Swagger",
+    id: 5,
+    title: "MongoDB",
+    percent: 65,
   },
 ];
 
+knowledges = [
+  {
+    id: 1,
+    title: "Git & Github",
+  },
+  {
+    id: 2,
+    title: "SEO",
+  },
+  {
+    id: 3,
+    title: "APIs",
+  },
+  {
+    id: 4,
+    title: "Hosting & Domains",
+  },
+  {
+    id: 5,
+    title: "Trello & Jira",
+  },
+  {
+    id: 6,
+    title: "JSON & AJAX",
+  },
+  {
+    id: 7,
+    title: "DOM & BOM",
+  },
+  {
+    id: 8,
+    title: "jQuery",
+  },
+  {
+    id: 9,
+    title: "Bootstrap",
+  },
+  {
+    id: 10,
+    title: "TailwindCSS",
+  },
+  {
+    id: 11,
+    title: "JWT",
+  },
+  {
+    id: 12,
+    title: "Postman",
+  },
+  {
+    id: 13,
+    title: "Docker",
+  },
+  {
+    id: 14,
+    title: "Swagger",
+  },
+];
 
-certifications = 
-[
+certifications = [
   {
     id: 0,
     name: "Concepteur Developpeur d'Application Full stack MERN",
-    imageUrl: "https://github.com/user-attachments/assets/6d953158-a120-4390-acba-528a07937671",
-    description: "A MERN Stack bootcamp from",
+    img: "https://github-production-user-asset-6210df.s3.amazonaws.com/73648971/350718733-6d953158-a120-4390-acba-528a07937671.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVCODYLSA53PQK4ZA%2F20240720%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240720T191120Z&X-Amz-Expires=300&X-Amz-Signature=b2ba2ea50ed7ccdc75df6f0f6b75f4aeefce7e200220c6864feb5ccb59944699&X-Amz-SignedHeaders=host&actor_id=73648971&key_id=0&repo_id=425853170",
     alt: "mern-stack-developer",
-    from: "Simplon",
-    url: "https://www.simplon.ma/",
+    src: "Simplon",
     date: "May 9, 2024",
+    verifyCert: "",
+    certID: "",
   },
   {
-    "id": 0,
-    "name": "Concepteur Developpeur d'Application Full stack MERN",
-    "img" : "https://user-images.githubusercontent.com/73648971/151952865-7c015e7d-5d02-4ed9-8dd4-e80ea54ba924.png",
-    "alt" : "mern-stack-developer",
-    "src": "Simplon",
-    "date": "May 9, 2024",
-    "verifyCert": "",
-    "certID":""
-  },
-   {
-    "id": 1,
-    "name": "Front-End Web UI Frameworks and Tools: Bootstrap 4",
-    "img" : "https://user-images.githubusercontent.com/73648971/151952865-7c015e7d-5d02-4ed9-8dd4-e80ea54ba924.png",
-    "alt" : "frontend-web-ui",
-    "src": "Coursera",
-    "date": "Jan 27, 2022",
-    "verifyCert": "https://www.coursera.org/verify/EWK8YY9A3NZX",
-    "certID":""
+    id: 1,
+    name: "Front-End Web UI Frameworks and Tools: Bootstrap 4",
+    img: "https://user-images.githubusercontent.com/73648971/151952865-7c015e7d-5d02-4ed9-8dd4-e80ea54ba924.png",
+    alt: "frontend-web-ui",
+    src: "Coursera",
+    date: "Jan 27, 2022",
   },
   {
-    "id": 2,
-    "name": "Build web apps with ReactJS",
-    "img" : "https://user-images.githubusercontent.com/73648971/151952297-a7f4a7ba-ff59-4619-a873-d8930322d737.png",
-    "alt" : "build-web-apps-with-react",
-    "src": "OpenClassRooms",
-    "date": "Dec 26, 2021",
-    "verifyCert": "https://openclassrooms.com/en/course-certificates/4108820132",
-    "certID":"4108820132"
+    id: 2,
+    name: "Build web apps with ReactJS",
+    img: "https://user-images.githubusercontent.com/73648971/151952297-a7f4a7ba-ff59-4619-a873-d8930322d737.png",
+    alt: "build-web-apps-with-react",
+    src: "OpenClassRooms",
+    date: "Dec 26, 2021",
+    verifyCert: "https://openclassrooms.com/en/course-certificates/4108820132",
+    certID: "4108820132",
   },
   {
-    "id": 3,
-    "name": "Computer Thinking",
-    "img" : "https://user-images.githubusercontent.com/73648971/151952391-6e616e1f-9488-49bf-a38b-8b28d91ba506.PNG",
-    "alt" : "computer-thinking",
-    "src": "OpenClassRooms",
-    "date": "Dec 10, 2021",
-    "verifyCert": "https://openclassrooms.com/en/course-certificates/2907010334",
-    "certID":"2907010334"
+    id: 3,
+    name: "Computer Thinking",
+    img: "https://user-images.githubusercontent.com/73648971/151952391-6e616e1f-9488-49bf-a38b-8b28d91ba506.PNG",
+    alt: "computer-thinking",
+    src: "OpenClassRooms",
+    date: "Dec 10, 2021",
+    verifyCert: "https://openclassrooms.com/en/course-certificates/2907010334",
+    certID: "2907010334",
   },
   {
-    "id": 4,
-    "name": "Responsive Web Design",
-    "img" : "https://user-images.githubusercontent.com/73648971/151952730-c617e186-0bcb-468e-b8d1-351c826ef5b6.jpg",
-    "alt" : "responsive-web-design",
-    "src": "FreeCodeCamp",
-    "date": "Dec 11, 2020",
-    "verifyCert": "https://freecodecamp.org/certification/hossam-dev14/responsive-web-design/",
-    "certID":""
+    id: 4,
+    name: "Responsive Web Design",
+    img: "https://user-images.githubusercontent.com/73648971/151952730-c617e186-0bcb-468e-b8d1-351c826ef5b6.jpg",
+    alt: "responsive-web-design",
+    src: "FreeCodeCamp",
+    date: "Dec 11, 2020",
+    verifyCert:
+      "https://freecodecamp.org/certification/hossam-dev14/responsive-web-design/",
+    certID: "",
   },
   {
-    "id": 5,
-    "name": "Java Course",
-    "img" : "https://user-images.githubusercontent.com/73648971/151953603-162b7484-846c-4fbb-8499-dfb3fbd1174c.png",
-    "alt" : "responsive-web-design",
-    "src": "SOLOLEARN",
-    "date": "Feb 27, 2019",
-    "verifyCert": "https://www.sololearn.com/Certificate/1068-4539606/jpg/",
+    id: 5,
+    name: "Java Course",
+    img: "https://user-images.githubusercontent.com/73648971/151953603-162b7484-846c-4fbb-8499-dfb3fbd1174c.png",
+    alt: "responsive-web-design",
+    src: "SOLOLEARN",
+    date: "Feb 27, 2019",
+    verifyCert: "https://www.sololearn.com/Certificate/1068-4539606/jpg/",
   },
-  
-  
-  
-  
 ];
 
-
-works = 
-[
+works = [
   {
-    "id": 0,
-    "img" : "https://github.com/user-attachments/assets/516aa752-2039-4a7a-8984-a31b5ac06a6e",
-    "alt" : "h-dev14",
-    "title" : "h-dev14 ",
-    "desx": "My NextJS portfoli website showcases my work and experience as a full-stack web developer.",
-    "prev_img" : "https://github.com/user-attachments/assets/04d71608-6c55-4f30-9ccd-b337087d3e2e",
-    "preview" : "https://h-dev14.vercel.app/"
-    "github" : "https://h-dev14.vercel.app/",
-  },
-{
-    "id": 1,
-    "img" : "https://raw.githubusercontent.com/hossam-dev14/tech_innovate/main/TechInnovate%20preveiw.png",
-    "alt" : "TechInnovate",
-    "title" : "TechInnovate",
-    "desx": "A portfoli website showcases my work and experience as a full-stack web developer.  ",
-    "prev_img" : "https://raw.githubusercontent.com/hossam-dev14/tech_innovate/main/TechInnovate%20preveiw.png",
-    "preview" : "https://tech-innovate-14.vercel.app/"
-    "github" : "https://github.com/hossam-dev14/tech_innovate",
+    id: 0,
+    img: "https://github.com/user-attachments/assets/516aa752-2039-4a7a-8984-a31b5ac06a6e",
+    alt: "h-dev14",
+    title: "h-dev14 ",
+    desx: "My NextJS portfoli website showcases my work and experience as a full-stack web developer.",
+    prev_img:
+      "https://github.com/user-attachments/assets/04d71608-6c55-4f30-9ccd-b337087d3e2e",
+    preview: "https://h-dev14.vercel.app/",
+    github: "https://h-dev14.vercel.app/",
   },
   {
-    "id": 2,
-    "img" : "https://raw.githubusercontent.com/hossam-dev14/ImmoMarket/main/immomarket_real_estate.png",
-    "alt" : "ImmoMarket",
-    "title" : "ImmoMarket",
-    "desc": "A dynamic real estate market, For real estate advertisements and rental of apartments and houses",
-    "prev_img" : "https://raw.githubusercontent.com/hossam-dev14/ImmoMarket/main/immomarket_real_estate.png",
-    "preview" : "https://immomarket.vercel.app/",
-    "github" : "https://github.com/hossam-dev14/ImmoMarket"
+    id: 1,
+    img: "https://raw.githubusercontent.com/hossam-dev14/tech_innovate/main/TechInnovate%20preveiw.png",
+    alt: "TechInnovate",
+    title: "TechInnovate",
+    desx: "A portfoli website showcases my work and experience as a full-stack web developer.  ",
+    prev_img:
+      "https://raw.githubusercontent.com/hossam-dev14/tech_innovate/main/TechInnovate%20preveiw.png",
+    preview: "https://tech-innovate-14.vercel.app/",
+    github: "https://github.com/hossam-dev14/tech_innovate",
   },
   {
-    "id": 3,
-    "img" : "https://user-images.githubusercontent.com/73648971/112833177-473f0400-908e-11eb-953a-a8d03d03fc33.jpg",
-    "alt" : "animal-blog",
-    "title" : "Animal Blog",
-    "prev_img" : "https://user-images.githubusercontent.com/73648971/151997109-5a7fdbbb-336e-4040-a07f-2a31bcd3a07c.jpg",
-    "preview" : "https://hossam-dev14.github.io/Animal-Blog/",
-    "github" : "https://github.com/hossam-dev14/Animal-Blog/"
+    id: 2,
+    img: "https://raw.githubusercontent.com/hossam-dev14/ImmoMarket/main/immomarket_real_estate.png",
+    alt: "ImmoMarket",
+    title: "ImmoMarket",
+    desc: "A dynamic real estate market, For real estate advertisements and rental of apartments and houses",
+    prev_img:
+      "https://raw.githubusercontent.com/hossam-dev14/ImmoMarket/main/immomarket_real_estate.png",
+    preview: "https://immomarket.vercel.app/",
+    github: "https://github.com/hossam-dev14/ImmoMarket",
   },
   {
-    "id": 4,
-    "img" : "https://user-images.githubusercontent.com/73648971/112232119-5100db80-8c38-11eb-9403-1c7396390c4b.png",
-    "alt" : "portfolio",
-    "title" : "Portfolio",
-    "prev_img" : "https://user-images.githubusercontent.com/73648971/151997336-9b6139d4-336a-4d0b-bd92-6b0f35d3d4ff.jpg",
-    "preview" : "https://hossam-dev14.github.io/Portfolio/",
-    "github" : "https://github.com/hossam-dev14/Portfolio/"
+    id: 3,
+    img: "https://user-images.githubusercontent.com/73648971/112833177-473f0400-908e-11eb-953a-a8d03d03fc33.jpg",
+    alt: "animal-blog",
+    title: "Animal Blog",
+    prev_img:
+      "https://user-images.githubusercontent.com/73648971/151997109-5a7fdbbb-336e-4040-a07f-2a31bcd3a07c.jpg",
+    preview: "https://hossam-dev14.github.io/Animal-Blog/",
+    github: "https://github.com/hossam-dev14/Animal-Blog/",
   },
   {
-    "id": 5,
-    "img" : "https://raw.githubusercontent.com/hossam-dev14/recipe_crud_api/main/Recipe_api.png",
-    "alt" : "Recipe_Rest_API",
-    "title" : "Recipe Restfull API",
-    "prev_img" : "https://github.com/hossam-dev14/recipe_crud_api/assets/73648971/1bb68823-cb99-4993-91c9-f431213b5fbb",
-    "preview" : "https://recipe-rest-api-fytu.onrender.com/api-docs/",
-    "github" : "https://github.com/hossam-dev14/recipe_crud_api"
+    id: 4,
+    img: "https://user-images.githubusercontent.com/73648971/112232119-5100db80-8c38-11eb-9403-1c7396390c4b.png",
+    alt: "portfolio",
+    title: "Portfolio",
+    prev_img:
+      "https://user-images.githubusercontent.com/73648971/151997336-9b6139d4-336a-4d0b-bd92-6b0f35d3d4ff.jpg",
+    preview: "https://hossam-dev14.github.io/Portfolio/",
+    github: "https://github.com/hossam-dev14/Portfolio/",
   },
-  
-]
+  {
+    id: 5,
+    img: "https://raw.githubusercontent.com/hossam-dev14/recipe_crud_api/main/Recipe_api.png",
+    alt: "Recipe_Rest_API",
+    title: "Recipe Restfull API",
+    prev_img:
+      "https://github.com/hossam-dev14/recipe_crud_api/assets/73648971/1bb68823-cb99-4993-91c9-f431213b5fbb",
+    preview: "https://recipe-rest-api-fytu.onrender.com/api-docs/",
+    github: "https://github.com/hossam-dev14/recipe_crud_api",
+  },
+];
